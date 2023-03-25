3c88997051e51c914b6fd2f540664a27d36c1b89
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2023-03-25 16:11:58 -0300

    feat: configurated stitches configs styles

---
+++ src/styles/index.ts
@@ -0,0 +1,42 @@
+import { createStitches } from '@stitches/react';
+
+const spaceObject = {
+  1: '0.25rem',
+  2: '0.5rem',
+  3: '0.75rem',
+  4: '1rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2rem',
+};
+
+export const { styled, getCssText } = createStitches({
+  media: {
+    sm: '(max-width: 767px)',
+    md: '(min-width: 768px) and (max-width: 1365px)',
+    lg: '(min-width: 1366px) and (max-width: 1439px)',
+    xl: '(min-width: 1440px)',
+  },
+  theme: {
+    fonts: { system: 'system-ui' },
+    colors: {
+      black1000: '#000',
+
+      blue700: '#3599F5',
+
+      gray600: '#454D4A',
+      gray500: '#B0B0B0',
+      gray400: '#454D4A15',
+
+      green700: '#03C68B',
+
+      red800: '#e61a4d',
+
+      white1000: '#FFFFFF',
+      white900: '#F1F0F0',
+    },
+    fontSizes: spaceObject,
+    space: spaceObject,
+  },
+});
