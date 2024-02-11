3b96d56e64686b72d3d74ba550e656742f07993f
bruno-alves-startse <bruno.alves@startse.com>
2024-02-11 01:14:17 -0300

    fix: page serverless function timeout error

--- src/pages/carrinho/index.tsx
+++ src/pages/carrinho/index.tsx
@@ -1,35 +1,12 @@
-import { GetServerSideProps, NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import React from 'react';
 
-import { CartController } from '@/infra/http/controllers/cart';
-import { CartProvider } from '@/providers/cart';
-import { ICartProduct } from '@/domain/cart/entities';
 import { CartPresentation } from '@/presentation/pages/cart';
-import { CART_PRODUCT_IDS } from '@/utils/constants';
 
-interface ICart {
-  products: Array<ICartProduct>;
-}
+const Cart: NextPage = () => <CartPresentation />;
 
-const Cart: NextPage<ICart> = props => (
-  <CartPresentation {...props} />
-);
-
-export const getServerSideProps: GetServerSideProps = async ctx => {
-  try {
-    const productsCookie = ctx.req.cookies[CART_PRODUCT_IDS];
-
-    const cartController = new CartController(new CartProvider());
-    const props = await cartController.index(
-      JSON.parse(productsCookie),
-    );
-
-    return { props };
-  } catch {
-    return {
-      props: { products: [] },
-    };
-  }
-};
+export const getStaticProps: GetStaticProps = () => ({
+  props: {},
+});
 
 export default Cart;
--- src/presentation/pages/cart/index.tsx
+++ src/presentation/pages/cart/index.tsx
@@ -1,21 +1,53 @@
 import { CartMenu } from '@/presentation/components/pages/cart/Menu';
 import { CartProducts } from '@/presentation/components/pages/cart/Products';
 import { ICartProduct } from '@/domain/cart/entities';
+import { useEffect, useState } from 'react';
+import { cookies } from '@/utils/cookies';
+import { CART_PRODUCT_IDS } from '@/utils/constants';
+import { CartController } from '@/infra/http/controllers/cart';
+import { CartProvider } from '@/providers/cart';
 
 interface ICartPresentation {
   products: Array<ICartProduct>;
 }
 
-const CartPresentation: React.FC<ICartPresentation> = ({
-  products,
-}) => (
-  <div className="sm:mx-2 sm:mt-4 flex flex-col m-[40px] w-full">
-    <div className="sm:flex-col flex flex-row w-full gap-4">
-      <CartProducts products={products} />
+const CartPresentation = () => {
+  const [products, setProducts] = useState<Array<ICartProduct>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-      <CartMenu products={products} />
+  const fetchCartProducts = async () => {
+    const productsCookie = cookies.get(CART_PRODUCT_IDS);
+    if (!productsCookie) return;
+
+    try {
+      const cartController = new CartController(new CartProvider());
+      const { products } = await cartController.index(productsCookie);
+
+      setProducts(products);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCartProducts();
+  }, []);
+
+  return (
+    <div className="sm:mx-2 sm:mt-4 flex flex-col m-[40px] w-full">
+      <div className="sm:flex-col flex flex-row w-full gap-4 justify-center">
+        {isLoading ? (
+          <h1 className="text-6 font-semibold">Carregando...</h1>
+        ) : (
+          <>
+            <CartProducts products={products} />
+
+            <CartMenu products={products} />
+          </>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { CartPresentation };
